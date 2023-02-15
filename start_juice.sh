278b31185737fab19ad8bcf37abee1e37e2e831c
Bernard Greyling <bernard@rampgrowth.com>
2023-02-15 14:08:41 +0200

    Update Juice script to allow for S3 context

--- start_juice.sh
+++ start_juice.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 source .env
 
+while getopts c: flag
+do
+    case "${flag}" in
+        c) context=${OPTARG};;
+    esac
+done
+
 resolve_deps() {
     if [[ $(type -P "$1") ]]
     then 
@@ -19,10 +26,23 @@ resolve_deps() {
 # Resolve dependencies
 resolve_deps juicefs "https://juicefs.com/docs/community/installation/"
 
-echo -e "\n Creating cache DB with name ${JUICE_LOCAL_NAME} \n"
-juicefs format sqlite3://${JUICE_LOCAL_NAME}.db ${JUICE_LOCAL_NAME}
+echo $context
+
+if [ $context == "s3" ]
+then
+    echo -e "\n Creating S3 cache DB with name ${JUICE_LOCAL_NAME} \n"
+    juicefs format --storage s3 \
+        --bucket $AWS_BUCKET_PATH \
+        --access-key $AWS_ACCESS_KEY_ID \
+        --secret-key $AWS_SECRET_ACCESS_KEY \
+        --session-token $AWS_SESSION_TOKEN \
+        sqlite3://${JUICE_LOCAL_NAME}.db ${JUICE_LOCAL_NAME}
+else
+    echo -e "\n Creating Local cache DB with name ${JUICE_LOCAL_NAME} \n"
+    juicefs format sqlite3://${JUICE_LOCAL_NAME}.db ${JUICE_LOCAL_NAME}
+fi
 
 echo -e "\nMounting cache at path ${JUICE_LOCAL_PATH} \n"
 juicefs mount sqlite3://${JUICE_LOCAL_NAME}.db $JUICE_LOCAL_PATH || \
 echo -e "\n A local storage file for JUICE_LOCAL_NAME=$JUICE_LOCAL_NAME already exists \n" && \
-echo -e "Either change JUICE_LOCAL_NAME=$JUICE_LOCAL_NAME or delete $HOME/.juicefs/local/$JUICE_LOCAL_NAME " \
+echo -e "Either change JUICE_LOCAL_NAME=$JUICE_LOCAL_NAME or delete $HOME/.juicefs/local/$JUICE_LOCAL_NAME " 
